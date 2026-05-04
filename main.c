dd41b7d81ecb54f41e5fefd9eccd77d2c81778e5
Tamer Abdel-fattah <marwants2006@gmail.com>
2026-05-04 11:37:35 +0300

    Added main.c with first program

--- main.c
+++ main.c
@@ -1,7 +1,47 @@
 #include <stdio.h>
 
-int main(void)
-{
-    printf("hello world\n");
+// Function declarations
+void showMenu();
+
+int main() {
+    int choice;
+
+    do {
+        showMenu();
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+            case 1:
+                printf("Stories section coming soon...\n\n");
+                break;
+
+            case 2:
+                printf("Games section coming soon...\n\n");
+                break;
+
+            case 3:
+                printf("Quiz section coming soon...\n\n");
+                break;
+
+            case 4:
+                printf("Exiting... Goodbye!\n");
+                break;
+
+            default:
+                printf("Invalid choice! Try again.\n\n");
+        }
+
+    } while(choice != 4);
+
     return 0;
+}
+
+// Menu function
+void showMenu() {
+    printf("===== Kids Magazine =====\n");
+    printf("1. Stories\n");
+    printf("2. Games\n");
+    printf("3. Quiz\n");
+    printf("4. Exit\n");
 }
